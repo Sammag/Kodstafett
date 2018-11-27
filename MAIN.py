80d538a790a6c3ce145d88704294abcdbd97d9bd
Samuel Magnusson <masa16@nti.umea>
2018-11-27 09:02:26 +0100

    Del 2

---
+++ MAIN.py
@@ -0,0 +1,12 @@
+choice = 0
+message = ""
+
+while choice != 3:
+    print("Välkomen till loggboken.")
+    print("1. Läs loggar")
+    print("2. Skriv en logg")
+    print("3. Avsluta")
+    choice = int(input())
+
+
+    
