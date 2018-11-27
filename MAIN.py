b0df83cc6a62fc35200bb095f76a0c0cfefcb0e2
Samuel Magnusson <masa16@nti.umea>
2018-11-27 10:31:42 +0100

    Del 5

--- MAIN.py
+++ MAIN.py
@@ -7,14 +7,30 @@ while choice != 3:
     print("2. Skriv en logg")
     print("3. Avsluta")
     choice = int(input())
-    if choice == 1: 
-        f = open('log.txt', 'r')   
+
+    if choice == 1:
+        f = open('log.txt', 'r')
         for line in f:
             print(line)
         f.close()
-    elif choice == 2:
-        print("dab")
-    else:  
-        print("bad")
 
-    
+    elif choice == 2:
+        f = open('log.txt', 'a+')
+        message = input("Skriv din nya rad: ")
+        f.write("\n" + message)
+        f.close()
+
+    else:
+        print("          | ")
+        print("　　　　　| ")
+        print("　　　　　| ")
+        print("　　　　　| ")
+        print("　　　　　| ")
+        print("　　　　　| ") 
+        print("　　　　　| ")
+        print("　／￣￣＼| ")
+        print("＜ ´･ 　　|＼ ")
+        print("　|　３　 | 丶＼ - ")
+        print("＜ 、･　　|　　＼ ")
+        print("　＼＿＿／∪ _ ∪) ")
+        print("　　　　　 Ｕ Ｕ")
